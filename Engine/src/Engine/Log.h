e22fff24cb857e7f50f0aaf03ccf2f2a7b7114de
Niels <n.eppenhof@gmail.com>
2021-10-07 00:27:46 +0200

    Fixed wrong spdlog api call

--- Engine/src/Engine/Log.h
+++ Engine/src/Engine/Log.h
@@ -22,10 +22,10 @@ namespace Engine {
 #define ENGINE_CORE_INFO(...) ::Engine::Log::getCoreLogger()->info(__VA_ARGS__)
 #define ENGINE_CORE_WARN(...) ::Engine::Log::getCoreLogger()->warn(__VA_ARGS__)
 #define ENGINE_CORE_ERROR(...) ::Engine::Log::getCoreLogger()->error(__VA_ARGS__)
-#define ENGINE_CORE_FATAL(...) ::Engine::Log::getCoreLogger()->fatal(__VA_ARGS__)
+#define ENGINE_CORE_FATAL(...) ::Engine::Log::getCoreLogger()->critical(__VA_ARGS__)
 
 #define ENGINE_TRACE(...) ::Engine::Log::getClientLogger()->trace(__VA_ARGS__)
 #define ENGINE_INFO(...) ::Engine::Log::getClientLogger()->info(__VA_ARGS__)
 #define ENGINE_WARN(...) ::Engine::Log::getClientLogger()->warn(__VA_ARGS__)
 #define ENGINE_ERROR(...) ::Engine::Log::getClientLogger()->error(__VA_ARGS__)
-#define ENGINE_FATAL(...) ::Engine::Log::getClientLogger()->fatal(__VA_ARGS__)
+#define ENGINE_FATAL(...) ::Engine::Log::getClientLogger()->critical(__VA_ARGS__)
