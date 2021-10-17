571e1bb8e68e2a5bc46f1c867804261b3e790b5e
Niels <n.eppenhof@gmail.com>
2021-10-17 20:01:32 +0200

    put definition for inlines back in header...

--- Engine/src/Engine/Renderer/Buffer.h
+++ Engine/src/Engine/Renderer/Buffer.h
@@ -11,7 +11,24 @@ namespace Engine {
 		None = 0, Float, Float2, Float3, Float4, Mat3, Mat4, Int, Int2, Int3, Int4, Bool
 	};
 
-	static unsigned int ShaderDataTypeSize(ShaderDataType type);
+	static unsigned int ShaderDataTypeSize(ShaderDataType type) {
+		switch (type) {
+			case ShaderDataType::Float:		return 4;
+			case ShaderDataType::Float2:	return 4 * 2;
+			case ShaderDataType::Float3:	return 4 * 3;
+			case ShaderDataType::Float4:	return 4 * 4;
+			case ShaderDataType::Mat3:		return 4 * 3 * 3;
+			case ShaderDataType::Mat4:		return 4 * 4 * 4;
+			case ShaderDataType::Int:		return 4;
+			case ShaderDataType::Int2:		return 4 * 2;
+			case ShaderDataType::Int3:		return 4 * 3;
+			case ShaderDataType::Int4:		return 4 * 4;
+			case ShaderDataType::Bool:		return 1;
+		}
+
+		ENGINE_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
 
 	// -----------------------------------------
 	//
@@ -29,7 +46,24 @@ namespace Engine {
 		BufferElement() {}
 		BufferElement(ShaderDataType type, const std::string& name, bool normalized = false) : name(name), type(type), size(ShaderDataTypeSize(type)), offset(0), normalized(normalized) {}
 
-		unsigned int getComponentCount() const;
+		unsigned int getComponentCount() const {
+			switch (type) {
+				case ShaderDataType::Float:		return 1;
+				case ShaderDataType::Float2:	return 2;
+				case ShaderDataType::Float3:	return 3;
+				case ShaderDataType::Float4:	return 4;
+				case ShaderDataType::Mat3:		return 3 * 3;
+				case ShaderDataType::Mat4:		return 4 * 4;
+				case ShaderDataType::Int:		return 1;
+				case ShaderDataType::Int2:		return 2;
+				case ShaderDataType::Int3:		return 3;
+				case ShaderDataType::Int4:		return 4;
+				case ShaderDataType::Bool:		return 1;
+			}
+
+			ENGINE_CORE_ASSERT(false, "Unknown ShaderDataType!");
+			return 0;
+		}
 	};
 
 	class BufferLayout {
--- Engine/src/Engine/Renderer/RenderCommand.h
+++ Engine/src/Engine/Renderer/RenderCommand.h
@@ -5,9 +5,17 @@
 namespace Engine {
 	class RenderCommand {
 	public:
-		inline static void setClearColor(const glm::vec4& color);
-		inline static void clear();
-		inline static void drawIndexed(const std::shared_ptr<VertexArray>& vertexArray);
+		inline static void setClearColor(const glm::vec4& color) {
+			s_rendererAPI->setClearColor(color);
+		}
+
+		inline static void clear() {
+			s_rendererAPI->clear();
+		}
+
+		inline static void drawIndexed(const std::shared_ptr<VertexArray>& vertexArray) {
+			s_rendererAPI->drawIndexed(vertexArray);
+		}
 
 	private:
 		static RendererAPI* s_rendererAPI;
