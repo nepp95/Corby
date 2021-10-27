09221cd7114f82bdcf5d75dcab1fceda31aa4eba
Niels <nielseppenhof@hotmail.com>
2021-10-27 20:45:23 +0200

    upd

--- Engine/src/Engine/Input/CameraController.h
+++ Engine/src/Engine/Input/CameraController.h
@@ -21,13 +21,13 @@ namespace Engine {
 		bool onWindowResized(WindowResizeEvent& event);
 
 	private:
-		OrthographicCamera m_camera;
-		glm::vec3 m_cameraPosition = { 0.0f, 0.0f, 0.0f };
-
 		float m_aspectRatio;
 		float m_zoomLevel = 1.0f;
+		OrthographicCamera m_camera;
 
 		bool m_rotation;
+
+		glm::vec3 m_cameraPosition = { 0.0f, 0.0f, 0.0f };
 		float m_cameraRotation = 0.0f;
 		float m_cameraTranslationSpeed = 5.0f;
 		float m_cameraRotationSpeed = 180.0f;
