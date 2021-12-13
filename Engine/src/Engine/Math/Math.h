536280cdee87d7517fbf45546b3a3f5a056e30b1
Niels <nielseppenhof@hotmail.com>
2021-12-13 19:10:46 +0100

    Engine/Math formatting

--- Engine/src/Engine/Math/Math.h
+++ Engine/src/Engine/Math/Math.h
@@ -2,6 +2,7 @@
 
 #include <glm/glm.hpp>
 
-namespace Engine::Math {
+namespace Engine::Math
+{
 	bool decomposeTransform(const glm::mat4& transform, glm::vec3& translation, glm::vec3& rotation, glm::vec3& scale);
 }
