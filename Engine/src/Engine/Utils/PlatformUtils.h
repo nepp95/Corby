0768e0a81ca371d3753ca016050115e158079369
Niels <nielseppenhof@hotmail.com>
2021-12-13 19:14:34 +0100

    Engine/Utils formatting

--- Engine/src/Engine/Utils/PlatformUtils.h
+++ Engine/src/Engine/Utils/PlatformUtils.h
@@ -3,8 +3,10 @@
 #include <optional>
 #include <string>
 
-namespace Engine {
-	class FileDialogs {
+namespace Engine
+{
+	class FileDialogs
+	{
 	public:
 		static std::optional<std::string> openFile(const char* filter);
 		static std::optional<std::string> saveFile(const char* filter);
