38aea365daaac4205960b713c41ba6935cdc11f6
Niels <nielseppenhof@hotmail.com>
2021-12-13 19:16:44 +0100

    Engine misc files formatting

--- Engine/src/engpch.h
+++ Engine/src/engpch.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #ifdef ENG_PLATFORM_WINDOWS
-	#ifndef NOMINMAX
-		// See github.com/skypjack/entt/wiki/Frequently-Asked-Questions#warning-c4003-the-min-the-max-and-the-macro
-		#define NOMINMAX
-	#endif
+#ifndef NOMINMAX
+// See github.com/skypjack/entt/wiki/Frequently-Asked-Questions#warning-c4003-the-min-the-max-and-the-macro
+#define NOMINMAX
+#endif
 #endif
 
 #include <iostream>
