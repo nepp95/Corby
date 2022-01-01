c8b31ffca09c4b4884cccf27a344e4c2f68b1e53
Niels <nielseppenhof@hotmail.com>
2022-01-01 16:10:06 +0100

    Added Premake to setup script

--- scripts/Setup.py
+++ scripts/Setup.py
@@ -6,12 +6,18 @@ import platform
 from SetupPython import PythonConfiguration as PythonRequirements
 PythonRequirements.Validate()
 
+from SetupPremake import PremakeConfiguration as PremakeRequirements
+
 from SetupVulkan import VulkanConfiguration as VulkanRequirements
 os.chdir('./../') # Change from devtools/scripts directory to root
+premakeInstalled = PremakeRequirements.Validate()
 VulkanRequirements.Validate()
 
-if platform.system() == "Windows":
-    print("\nRunning premake...")
-    subprocess.call([os.path.abspath("./scripts/Win-GenProjects.bat"), "nopause"])
+if (premakeInstalled):
+    if platform.system() == "Windows":
+        print("\nRunning premake...")
+        subprocess.call([os.path.abspath("./scripts/Win-GenProjects.bat"), "nopause"])
 
-print("\nSetup completed!")
+    print("\nSetup completed!")
+else:
+    print("\nEngine requires Premake to generate project files!")
